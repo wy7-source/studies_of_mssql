502e43ca3bb83ddc83a9a1410b11ebf591b87f62
wy7-source <wythorbazan@gmail.com>
2020-07-26 15:49:38 -0300

    Selects com Order By

--- Commands/DQL_Examples.sql
+++ Commands/DQL_Examples.sql
@@ -1,7 +1,7 @@
 -- ABOUT Select's
 -- Bem estilo Mysql/PostgreSQL/OracleDB...
-
 use db_Biblioteca
+
 -- Simples
 SELECT Nome_Autor FROM tbl_autores;
 SELECT * FROM tbl_autores;
@@ -9,3 +9,11 @@ SELECT Nome_Livro FROM tbl_livro;
 SELECT Nome_Livro, Preco_Livro FROM tbl_livro;
 SELECT Nome_Livro, ISBN
 FROM tbl_livro;
+
+-- Usando OrderBy
+SELECT * FROM tbl_livro
+ ORDER BY Nome_Livro ASC;
+SELECT Nome_Livro, ID_Editora FROM tbl_livro
+ ORDER BY ID_Editora;
+SELECT Nome_Livro, Preco_Livro FROM tbl_livro
+ ORDER BY Preco_Livro DESC;
